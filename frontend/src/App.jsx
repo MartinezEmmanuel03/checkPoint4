189b3905bcc41b5963fa379ec8a31b413f874b2c
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2023-02-03 15:54:07 +0100

    mise en place design et liens pour la page prets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Public from "@pages/layouts/Public";
 import Register from "@pages/Register";
 import Login from "@pages/Login";
 import LivresForm from "@pages/LivresForm";
+import Prets from "@pages/Prets";
 import Home from "./pages/Home";
 
 import "./App.css";
@@ -18,6 +19,7 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="livresForm" element={<LivresForm />} />
+          <Route path="prets" element={<Prets />} />
         </Route>
       </Routes>
     </Router>
