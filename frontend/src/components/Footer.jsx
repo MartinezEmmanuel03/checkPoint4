6a209f91d7191b0064bcaf6899934d670fb43cd7
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2023-02-06 10:29:12 +0100

    petit bonus

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,12 @@ function Footer() {
     <div className="bg-grey text-white">
       <div className="flex flex-col pl-4 pt-2 mb-12">
         <h1 className="text-xl">BIBLIO-MIMOSA</h1>
-        <p className="hover:underline">à propos</p>
+        <a
+          href="https://www.youtube.com/watch?v=v7ScGV5128A"
+          className="hover:underline"
+        >
+          à propos
+        </a>
       </div>
       <p className="text-center pb-4">
         Copyright - BIBLIO-MIMOSA - Tous droits réservés
